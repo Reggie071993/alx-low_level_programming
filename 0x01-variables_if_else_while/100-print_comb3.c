0e073eb137e8de0616354557c9c48402802a6ac0
Reggie071993 <reykakwela@yahoo.com>
2022-03-13 14:41:14 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a = 49;
+	int b = 48;
+	int position_a = 49;
+	/* your code goes there */
+	while (b <= 56)
+	{
+		while (a <= 57)
+		{
+			putchar (b);
+			putchar (a);
+			if (b != 56 || a != 57)
+			{
+				putchar (',')
+				putchar (' ')
+			}
+			a++;
+		}
+		b++;
+		position_a++;
+		a = position_a;
+	}
+	putchar('\n');
+	return (0);
+}
