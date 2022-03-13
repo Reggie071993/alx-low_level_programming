8021ca69950912fe89a8b3ed550d3aaf0de64fdb
Reggie071993 <reykakwela@yahoo.com>
2022-03-13 15:06:44 +0000

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a = 50;
+	int b = 49;
+	int c = 48;
+	int position_a = a;
+	int position_b = b;
+	int position_aa = a;
+	/* your code goes there */
+	while (c <= 55)
+	{
+		while (b <= 56)
+		{
+			while (a <= 57)
+			{
+				putchar (c);
+				putchar (b);
+				putchar (a);
+				if (c != 55 || b != 56 || a != 57)
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			a++;
+			}
+			b++;
+			position_a++;
+			a = position_a;
+		}
+		position_a = position_aa;
+		position_a++;
+		position_aa++;
+		a = position_a;
+		position_b++;
+		b = position_d;
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
