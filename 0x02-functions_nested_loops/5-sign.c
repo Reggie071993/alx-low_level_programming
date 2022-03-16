3e5daf075074f0ccc63ee1ca7cb70058855e6431
Reggie071993 <reykakwela@yahoo.com>
2022-03-16 09:16:08 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 * print_sign - Determines integer is positive, negative or zero
 * @n: The integer
 *
-*Return 1 if positive, 0 if zero, -1 if negative
+*Return: 1 if positive, 0 if zero, -1 if negative
 */
 
 int print_sign(int n)
