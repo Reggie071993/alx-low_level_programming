b4bb6a44f33368b3c86ef8cdaf24f3538e464ac0
Reggie071993 <reykakwela@yahoo.com>
2022-03-17 13:13:37 +0000

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - check the code 
+* main - check the code
 *
 * Return: Always 0.
 */
@@ -8,8 +8,8 @@
 int main(void)
 {
 	int i;
-	
 	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
