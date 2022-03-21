87bb209e0b09f88f8d92b585b1d177174a55e7a1
Reggie071993 <reykakwela@yahoo.com>
2022-03-21 17:10:19 +0000

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* puts2 - takes a pointer to an int as parameter
+* @str: chain of characters
+* 
+* Return: 1 or 0
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n')
+}
