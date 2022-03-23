a92efe430eabca7a9b29f9c0a5d6765a09377af3
Reggie071993 <reykakwela@yahoo.com>
2022-03-23 20:16:24 +0000

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /* main - Entry point */
-
 int main(void)
 {
 	int n;
@@ -11,6 +10,7 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of code here...
+	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
