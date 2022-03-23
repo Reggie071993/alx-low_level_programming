2959167ce216ec19cb93371238f1923281b68724
Reggie071993 <reykakwela@yahoo.com>
2022-03-23 19:08:06 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,9 +6,9 @@
 * Return: 1 or 0
 */
 
-int _strcmp)char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && (*s1 == *s2)
+	while (*s1 != '\0' && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
