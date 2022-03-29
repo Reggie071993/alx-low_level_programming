e606f45cee4fd30e1c061251d8e096a23487e37c
Reggie071993 <reykakwela@yahoo.com>
2022-03-29 13:55:38 +0000

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,18 +5,18 @@
 * print_diagsums - a function that print the sum of two diagonals
 * @a: matrix
 * @size: length of matrice
-* Return: 1 or 0
+* Return: void
 */
 
 void print_diagsums(int *a, int size)
 {
 	int row, pos;
-	int sum1 - 0;
-	int sum2 - 0;
+	int sum1 = 0;
+	int sum2 = 0;
 
-	for (pos = a, row = 0; row < size; ++row, pos +=size + 1)
+	for (pos = a, row = 0; row < size; ++row, pos += size + 1)
 		sum1 += *pos;
-	for (pos = a + size - 1, row < size; ++row, pos += size - 1)
+	for (pos = a + size - 1, row = 0; row < size; ++row, pos += size - 1)
 		sum2 += *pos;
 
 	printf("%d, %d\n", sum1, sum2);
