09dc73093fa998aa57bceaec76d2d5f539d3d32b
Reggie071993 <reykakwela@yahoo.com>
2022-04-14 15:31:36 +0000

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,10 +14,15 @@ typedef struct print
 	void (*f)(va_list);
 } print_t;
 
+
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
 #endif /* _VARIADIC_FUNCTIONS_H_ */
