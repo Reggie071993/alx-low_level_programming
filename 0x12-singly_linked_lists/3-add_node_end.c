a78b6aab9bdb0e23c4b5ca5972f10909498285b1
Reggie071993 <reykakwela@yahoo.com>
2022-04-21 15:32:03 +0000

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,11 +3,12 @@
 #include <string.h>
 
 /**
- * _strlen - finds the length of a string
- * @str: string to find the length of
- *
- * Return: length of string
- */
+* _strlen - finds the length of a string
+* @str: string to find the length of
+* Return: length of string
+*/
+
+
 unsigned int _strlen(char *str)
 {
 	unsigned int i;
@@ -18,12 +19,12 @@ unsigned int _strlen(char *str)
 }
 
 /**
- * add_node_end - adds a new node to the end of linked list
- * @head: double pointer to a linked list
- * @str: string to add to the new node
- *
- * Return: pointer to the new node
- */
+* add_node_end - add new node to end of linked list
+* @head: double pointr to linked list
+* @str: string to add
+* Return: pointer to new node
+*/
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *tmp;
@@ -34,21 +35,26 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
+
 	if (new->str == NULL)
 	{
-		return (NULL);
 		free(new);
+		return (NULL);
 	}
+
 	new->len = _strlen(new->str);
 	new->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
+
 	tmp = *head;
 	while (tmp->next)
 		tmp = tmp->next;
 	tmp->next = new;
+
 	return (new);
 }
