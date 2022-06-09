b09e69be93e9220f6bf5ebfa0369d2d1d93e17c2
Reggie071993 <reykakwela@yahoo.com>
2022-06-09 22:57:44 +0000

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,55 +1,52 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Add node at nth index
- *
- * @h: Head of node
- *
- * @idx: index
- *
- * @n: struct int
- *
- * Return: dlistint_t
+ * insert_dnodeint_at_index - inserts node at index
+ * @h: head of node
+ * @idx: index to insert node
+ * @n: data for new node
+ * Return: list with inserted node
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *current;
-	unsigned int count = 0;
+	unsigned int count = 1;
+	dlistint_t *temp = NULL, *new = NULL;
 
-	if (h == NULL || new_node == NULL)
-	{
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL || h == NULL)
 		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	current = *h;
-
+	new->n = n;
+	temp = *h;
 	if (idx == 0)
 	{
-		new_node = add_dnodeint(h, n);
-		return (new_node);
+		*h = new;
+		new->next = temp;
+		new->prev = NULL;
+		temp->prev = new;
+		return (new);
 	}
-	while (current)
+	while (temp->next != NULL)
 	{
-		if (current->next == NULL && count == idx - 1)
+		if (count == idx) /* found back */
 		{
-			new_node = add_dnodeint_end(h, n);
-			return (new_node);
+			new->prev = temp; /* current prev to back link */
+			new->next = temp->next; /* current next to front link*/
+			temp->next = new; /* back next link */
+			new->next->prev = new; /* from prev link */
 		}
-		else if ((idx - 1) == count)
-		{
-			new_node->next = current->next;
-			new_node->prev = current;
-			current->next->prev = new_node;
-			current->next = new_node;
-			return (new_node);
-		}
+		temp = temp->next;
 		count++;
-		current = current->next;
 	}
-	free(new_node);
-	return (NULL);
+	if (count == idx) /* end of DLL */
+	{
+		new->prev = temp; /* current prev to back link */
+		new->next = NULL; /* current next to NULL*/
+		temp->next = new; /* back next link */
+	}
+	if (count < idx)
+	{
+		free(new);
+		return (NULL);
+	}
+	return (new);
 }
